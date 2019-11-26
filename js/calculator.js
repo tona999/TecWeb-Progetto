5e52953d47b207148231e15d63ccf4a2afba677a
Antonio <antonio.zlatkovski438gmail.com>
2019-11-26 23:52:00 +0100

    minor fixes, receipt overview, basse for receipt VIEW, checking sample data validity and input format needs to be made better

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,15 @@
+var sampleIngredientCopy;
+var totalGramsLastChanged = true;
 function start()
 {
+	sampleIngredientCopy = document.getElementById("sampleIngredient").cloneNode(true);
+	document.getElementById("sampleIngredient").remove();
 	addIngredient();
 }
 
 function addIngredient()
 {
-	var sampleIngredient = document.getElementById("sampleIngredient");
-	var ing = sampleIngredient.cloneNode(true);
+	var ing = sampleIngredientCopy.cloneNode(true);
 	ing.removeAttribute("id");
 	document.getElementById("receipt").appendChild(ing);
 
@@ -46,6 +49,39 @@ function addIngredient()
 	ing.carbsPP.removeAttribute("id");
 } 
 
+function refreshReceiptData()
+{
+	var receipt = document.getElementById("receipt");
+
+	var totalReceiptGrams = 0;
+	var totalReceiptCarbs = 0;
+
+	for (var i=0; i<receipt.childNodes.length; i++){
+		totalReceiptGrams = totalReceiptGrams + toInteger(receipt.childNodes[i].totalGrams.value);
+	}
+
+	document.getElementById("totalReceiptGrams").innerHTML = totalReceiptGrams;
+	document.getElementById("totalReceiptCarbs");
+}
+
+function toInteger(str)
+{
+	if (str == "")
+		return 0;
+	if (!isNaN(str))
+		return parseInt(str);
+	return 0;
+}
+
+function toFloat(str)
+{
+	if (str == "")
+		return 0;
+	if (!isNaN(str))
+		return parseFloat(str).toFixed(2);
+	return 0;
+}
+
 //Events & Setters --------------------------------------------
 function onCloseClicked(ingredient){
 	ingredient.remove();
@@ -53,53 +89,61 @@ function onCloseClicked(ingredient){
 
 function onSampleGramsChanged(ingredient) {
   	var sg = ingredient.sampleGrams;
-  	if (sg.value <= 0)
+  	if (toInteger(sg.value) <= 0)
 		sg.value = "";
 	else
-		sg.value = parseFloat(sg.value).toFixed(0);
+		sg.value = toInt(sg.value);
 	
 	checkMaxSize(sg);
 	checkSampleDataValidity(ingredient, true);
+	calculateSecondRow(ingredient, totalGramsLastChanged);
+	calculateThirdRow(ingredient);
 }
 
 function onSampleCarbsChanged(ingredient){
   	var sc = ingredient.sampleCarbs;
-  	if (sc.value <= 0)
+  	if (toInteger(sc.value) <= 0)
 		sc.value = "";
 
 	checkMaxSize(sc);
 	checkSampleDataValidity(ingredient, false);
+	calculateSecondRow(ingredient, totalGramsLastChanged);
+	calculateThirdRow(ingredient);
 }
 
 function onTotalGramsChanged(ingredient){
+	totalGramsLastChanged = true;
   	var tg = ingredient.totalGrams;
-  	if (tg.value <= 0)
+  	if (toInteger(tg.value) <= 0)
 		tg.value = "";
 
 	checkMaxSize(tg);
 	calculateSecondRow(ingredient, true);
+	calculateThirdRow(ingredient);
 }
 
 function onTotalBreadUnitsChanged(ingredient){
+	totalGramsLastChanged = false;
   	var tbu = ingredient.totalBreadUnits;
-  	if (tbu.value <= 0)
+  	if (toFloat(tbu.value) <= 0)
 		tbu.value = "";
 
 	checkMaxSize(tbu);
 	calculateSecondRow(ingredient, false);
+	calculateThirdRow(ingredient);
 }
 
 function onPiecesNumberChanged(ingredient){
 	var pn = ingredient.piecesNumber;
-	if (pn.value <= 0)
-		pn.value = "0";
+	if (toInteger(pn.value) <= 0)
+		pn.value = "";
 
 	calculateThirdRow(ingredient);
 }
 
 function onSignClicked(ingredient, increment){
 	var pn = ingredient.piecesNumber;
-	pn.value = parseInt(ingredient.piecesNumber.value) + increment;
+	pn.value = toInteger(ingredient.piecesNumber.value) + increment;
 
 	onPiecesNumberChanged(ingredient);
 }
@@ -110,7 +154,7 @@ function checkSampleDataValidity(ingredient, sampleGramsChanged)
 	var sg = ingredient.sampleGrams;
 	var sc = ingredient.sampleCarbs;
 
-	if (parseInt(ingredient.sampleCarbs.value) > parseInt(ingredient.sampleGrams.value))
+	if (toInteger(ingredient.sampleCarbs.value) > toInteger(ingredient.sampleGrams.value))
 	{
 		if (sampleGramsChanged){
 			sg.className = "invalidInput";
@@ -138,31 +182,36 @@ function checkMaxSize(field)
 
 //Modifiers--------------------------------------------------------------------
 
-function calculateSecondRow(ingredient, totalCarbsChanged)
+function calculateSecondRow(ingredient, totalGramsChanged)
 {
-	if (totalCarbsChanged == true)
-		ingredient.totalBreadUnits.value = (totalGramsToBreadUnits(ingredient)).toFixed(1);
+	if (!isSampleRowValid(ingredient))
+		return;
+
+	if (totalGramsChanged == true)
+		ingredient.totalBreadUnits.value = toFloat(totalGramsToBreadUnits(ingredient));
 	else
-		ingredient.totalGrams.value = (totalBreadUnitsToGrams(ingredient)).toFixed(1);
+		ingredient.totalGrams.value = toFloat(totalBreadUnitsToGrams(ingredient));
 }
 
 function calculateThirdRow(ingredient)
 {
-	pieces = ingredient.piecesNumber.value;
+	if (!isSampleRowValid(ingredient))
+		return;
+
+	pieces = toInteger(ingredient.piecesNumber.value);
 	if (pieces > 0)
 	{
 		totalCarbs = (ingredient.totalGrams.value*ingredient.sampleCarbs.value)/ingredient.sampleGrams.value;
 
-		ingredient.gramsPP.innerHTML = (ingredient.totalGrams.value/pieces).toFixed(1);
-		ingredient.carbsPP.innerHTML = (totalCarbs/pieces).toFixed(1);
-		ingredient.breadUnitsPP.innerHTML = (carbsToBreadUnits(totalCarbs)/pieces).toFixed(1);
+		ingredient.gramsPP.innerHTML = toFloat(ingredient.totalGrams.value/pieces);
+		ingredient.carbsPP.innerHTML = toFloat(totalCarbs/pieces);
+		ingredient.breadUnitsPP.innerHTML = toFloat(carbsToBreadUnits(totalCarbs)/pieces);
 	}
 	else
 	{
 		ingredient.gramsPP.innerHTML = 0.0;
 		ingredient.carbsPP.innerHTML = 0.0;
 		ingredient.breadUnitsPP.innerHTML = 0.0;
-
 	}
 }
 
@@ -176,6 +225,11 @@ function totalBreadUnitsToGrams(ingredient)
 {
 	totalCarbs = breadUnitsToCarbs(ingredient.totalBreadUnits.value);
 	return (totalCarbs*ingredient.sampleGrams.value)/ingredient.sampleCarbs.value;
+}
+
+function isSampleRowValid(ingredient)
+{
+	return !(toInteger(ingredient.sampleGrams.value) == 0 || toInteger(ingredient.sampleCarbs.value) == 0);
 }
 
 //Utilities----------------------------------------------------------------
