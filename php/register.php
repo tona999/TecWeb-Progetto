7515453695d0451fb91ac2873ee09bbc193293bc
Kero2375 <f.carboni98@gmail.com>
2020-01-24 13:07:26 +0100

    Fixed Registration + Credentials

--- php/register.php
+++ php/register.php
@@ -1,30 +1,79 @@
 <?php
 
+
+
+/* TODO: save the hashed password on db */
+
 require_once("connection.php");
 
 if(!isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["password2"]) || !isset($_POST["name"]) || !isset($_POST["surname"]) || !isset($_POST["birthdate"])) {
     header("Location: ../registerPage.php");
 }
 
-
 $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-$password = $_POST["password"];
+$password = $_POST["password1"];
 $password2 = $_POST["password2"];
 $name = $_POST["name"];
 $surname = $_POST["surname"];
 $birthdate = $_POST["birthdate"];
+$dateFormatted = date('Y-m-d', strtotime($_POST["birthdate"]));
 
 
-/* Email sanitize and check */
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    header("Location: ../registerPage.php?err=email");
-    die;
+function validateDate($dateFormatted){
+  $months = [31,28,31,30,31,30,31,31,30,31,30,31];
+  $datePattern = '/^(?<year>[0-9]{4})[\/|-](?<month>[0-9]{1,2})[\/|-](?<day>[0-9]{1,2})$/';
+  preg_match_all($datePattern, $dateFormatted, $matches);
+
+
+  if ($matches["year"][0] < (date('Y') - 3) && $matches["year"][0] > (date('Y')-150)) {
+    if ($matches["month"][0] < 13 && $matches["month"][0] > 0 ) {
+      if ($matches["year"][0]%4 == 0) {
+        $months[$matches["month"][0]-1]++;
+      }
+      if ($matches["day"][0] < $months[$matches["month"][0]-1] && $matches["day"][0] > 0) {
+        return;
+      }
+    }
+  }
+
+  header("Location: ../registerPage.php?err=date");
+  die;
+
 }
-// check if the passwords are matching
-if($password != $password2){
-    header("Location: ../registerPage.php?err=password");
-    die;
+
+function validateName($name) {
+  $namePattern = '/^[a-zA-Z]+(([\',. -][a-zA-Z ])?[a-zA-Z]*)*$/';
+  if (preg_match($namePattern,$name)) {
+    return;
+  }
+  header("Location: ../registerPage.php?err=nameOrSurname");
+  die;
 }
+
+function validateEmail($email) {
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      header("Location: ../registerPage.php?err=email");
+      die;
+  }
+}
+
+function validatePassword($password1,$password2) {
+  if($password1 != $password2){
+      header("Location: ../registerPage.php?err=password");
+      die;
+  } else if (strlen($password1)< 6 || strlen($password1) > 15){
+      header("Location: ../registerPage.php?err=passwordFormat");
+      die;
+  }
+}
+
+
+validateName($name);
+validateName($surname);
+validateEmail($email);
+validateDate($dateFormatted);
+validatePassword($password, $password2);
+
 
 $checkDuplicateEmail = $mysql->query(
     "SELECT * FROM User
@@ -38,7 +87,7 @@ if($checkDuplicateEmail->num_rows > 0) {
 // insertion query
 $insert = $mysql->query(
     "INSERT INTO User (Name, Surname, Email, Password_hash, Admin, Birthdate)
-    VALUES ('$name', '$surname','$email','$password',false,'$birthdate')");
+    VALUES ('$name', '$surname','$email','$password',false,'$dateFormatted')");
 
 // check if has been inserted and gives the Id for this session
 $isInserted = $mysql->query(
--- registerPage.php
+++ registerPage.php
@@ -3,12 +3,17 @@ require_once("php/page.php");
 $html = new Page();
 $html->setTitle("Register");
 $html->setMeta("");
-$html->setLogged();
 $html->setBodyPath("html/register.html");
 
 //checks for errors in login
 if(isset($_GET["err"])){
-    if($_GET["err"]=="pass"){
+    if($_GET["err"]=="nameOrSurname"){
+      $html->body = str_replace(
+          "<ERROR/>",
+          "<span>Invalid Name or Surname!</span>",
+          $html->body);
+    }
+    else if($_GET["err"]=="pass"){
         $html->body = str_replace(
             "<ERROR/>",
             "<span>Wrong username or password!</span>",
@@ -20,10 +25,22 @@ if(isset($_GET["err"])){
             "<span>Invalid email!</span>",
             $html->body);
     }
+    else if($_GET["err"]=="date"){
+        $html->body = str_replace(
+            "<ERROR/>",
+            "<span>Invalid Date: use the formate yyyy/mm/dd</span>",
+            $html->body);
+    }
     else if($_GET["err"]=="password"){
         $html->body = str_replace(
             "<ERROR/>",
             "<span>Passwords don't match</span>",
+            $html->body);
+    }
+    else if($_GET["err"]=="passwordFormat"){
+        $html->body = str_replace(
+            "<ERROR/>",
+            "<span>Password must contain between 6 and 15 characters</span>",
             $html->body);
     }
     else if($_GET["err"]=="emailInUse"){
