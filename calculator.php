bbd6b29d079a4cb9cab5fe8c670f67d9c9f73062
Antonio <antonio.zlatkovski438gmail.com>
2020-03-08 22:41:36 +0100

    close #12

--- calculator.php
+++ calculator.php
@@ -8,20 +8,20 @@ $html->setMeta(
     '<script src="js/MathUtilities.js"></script>'.
     '<script src="js/Recipe.js"></script>'.
     '<link rel="stylesheet" type="text/css" href="styles/calculator.css">');
-$html->setBodyPath("html/calculator.html");
-$html->noFooter();
+    $html->noFooter();
 
-//set body for starting on load
-$html->header = str_replace(
-    '<body onload="',
-    '<body onload="start(); ',
-    $html->header);
-$html->header = str_replace(
-    '<body>',
-    '<body onload="start();">',
-        $html->header);
+if (isset($_SESSION["userId"]))
+{
+    $html->setBodyPath("html/calculator.html");
 
-$html->body = str_replace("<_SAMPLE_INGREDIENT/>", file_get_contents("html/sampleIngredient.html"), $html->body);
+    //set body for starting on load
+    $html->header = str_replace('<body>','<body onload="start();">',$html->header);
+    $html->body = str_replace("<_SAMPLE_INGREDIENT/>", file_get_contents("html/sampleIngredient.html"), $html->body);
+}
+else
+{
+    $html->body = "<h1>PLEASE <a href='register.php'>REGISTER</a> AND <a href='login.php'>LOG IN</a> TO USE OUR CALCULATOR.</h1>";
+}
 
 $html->printHtml();
 ?>
