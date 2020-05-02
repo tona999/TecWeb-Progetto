0e0a4d6abb4d26ff54b67944885dc1388dc52068
Kero2375 <f.carboni98@gmail.com>
2020-05-02 17:06:17 +0200

    faq style and structure

--- commonQuestions.php
+++ commonQuestions.php
@@ -1,7 +1,7 @@
 <?php
 require_once("php/page.php");
 $html = new Page();
-$html->setTitle("Home");
+$html->setTitle("FAQ");
 $html->setMeta("");
 $html->setBodyPath("html/commonQuestions.html");
 $html->printHtml();    
